a6ea21db1f1032a59b679e38fcd365a7a130596b
Pedro Alves <alvespedro.dev@gmail.com>
2020-01-25 01:34:44 -0300

    Commit inicial

---
+++ Assets/Script/ImputFoguete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImputFoguete : MonoBehaviour
+{
+
+    public float vel = 6.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(x * Time.deltaTime * vel, y * Time.deltaTime * vel));
+    }
+}
---
+++ Assets/Script/criaTiro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class criaTiro : MonoBehaviour
+{
+    public GameObject canhao;
+    public GameObject tiro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(tiro, new Vector3(canhao.transform.position.x, canhao.transform.position.y, canhao.transform.position.z), canhao.transform.rotation);
+        }
+    }
+}
